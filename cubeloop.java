ff57bc34a363d1a64f1fc0c43545bfc7f0f74dc7
KamaleshMani <ffkamalesh444@gmail.com>
2025-04-01 20:05:13 +0530

    this is from kamalesh

--- cubeloop.java
+++ cubeloop.java
@@ -1,9 +1,12 @@
 public class cubeloop {
     public static void main(String[] args) {
         int n = 20;
-        for(int i = 0 ; i<=20 ; i++){
+        for(int i = 0 ; i<=n ; i++){
             
                 System.out.println(i*i*i);
+
+    
+                
             
         }
     }
